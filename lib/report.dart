48ec05abb935106afbe517427a6566de0818aa3b
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-06-19 20:29:40 +0530

    minor

--- lib/report.dart
+++ lib/report.dart
@@ -1,7 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Report extends StatelessWidget {
-  const Report({Key? key}) : super(key: key);
+class Report extends StatefulWidget {
+  @override
+  State<Report> createState() => _ReportState();
+}
+
+class _ReportState extends State<Report> {
+  var data;
+
+  @override
+  void initState() async {
+    CollectionReference _collectionRef =
+        FirebaseFirestore.instance.collection('payments');
+    QuerySnapshot querySnapshot = await _collectionRef.get();
+    final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+    print(allData);
+    setState(() {
+      data = allData;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
